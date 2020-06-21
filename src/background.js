007fb6dbf1cd7386044fffa11b6b11a1ac87faa9
Shabbir Hussain <shabbir.ahussain@gmail.com>
2020-06-20 17:30:31 -0700

    Adding working code.

---
+++ src/background.js
@@ -0,0 +1,16 @@
+chrome.tabs.onUpdated.addListener(createANewTabIfNoneExist);
+chrome.tabs.onRemoved.addListener(createANewTabIfNoneExist);
+
+function createANewTabIfNoneExist() {
+    let queryInfo = {active : false, currentWindow : true, title : "New Tab"}
+    window.setTimeout(function() {
+        chrome.tabs.query(queryInfo, function(result) {
+            if(result.length == 0) {
+                let createProperties = {active : false};
+                chrome.tabs.create(createProperties, function(){});
+            }
+        })
+    }, 3000);
+}
+
+
